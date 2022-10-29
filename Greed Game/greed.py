7a09238cb23928df5b550effd59413d4258ca0d7
mjburn0428 <113367122+mjburn0428@users.noreply.github.com>
2022-10-29 11:50:58 -0600

    Add files via upload

---
+++ Greed Game/greed.py
@@ -0,0 +1,57 @@
+from game.casting.actor import Actor
+from game.casting.cast import Cast
+
+from game.directing.director import Director
+
+from game.services.keyboard_service import KeyboardService
+from game.services.video_service import VideoService
+
+from game.shared.color import Color
+from game.shared.point import Point
+
+#Frame Rate controls speed of game. Lower the number the slower the speed. Higher the number the faster the speed.
+FRAME_RATE = 25
+#This is the height and width of the game display.
+MAX_X = 900
+MAX_Y = 600
+CELL_SIZE = 15
+FONT_SIZE = 15
+COLS = 60
+ROWS = 40
+CAPTION = "Greed"
+WHITE = Color(255, 255, 255)
+DEFAULT_ARTIFACTS = 40
+
+def main():
+    
+    # create the cast
+    cast = Cast()
+    
+    # create the banner
+    banner = Actor()
+    banner.set_text("")
+    banner.set_font_size(FONT_SIZE + 10)
+    banner.set_color(WHITE)
+    banner.set_position(Point(CELL_SIZE, 0))
+    cast.add_actor("banners", banner)
+    
+    # create the robot
+    x = int(MAX_X / 2)
+    y = MAX_Y - CELL_SIZE
+    position = Point(x, y)
+
+    robot = Actor()
+    robot.set_text("#")
+    robot.set_font_size(FONT_SIZE)
+    robot.set_color(WHITE)
+    robot.set_position(position)
+    cast.add_actor("robots", robot)
+
+    # start the game
+    keyboard_service = KeyboardService(CELL_SIZE)
+    video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
+    director = Director(keyboard_service, video_service, DEFAULT_ARTIFACTS, COLS, ROWS, FONT_SIZE, CELL_SIZE)
+    director.start_game(cast)    
+
+if __name__ == "__main__":
+    main()
